6c6aba97006f07d7e8f78c3f101715365e576384
Dhanush Reddy Chilakala <dhanush.actual1@gmail.com>
2022-12-01 10:19:49 -0600

    Day 1

---
+++ day-1/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+fs.readFile('db.txt', 'utf-8', (err, data) => {
+    if (err) return console.error(err);
+    let arr = data.split('\n');
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].split('\r')[0]
+    }
+
+    let calTracker = [];
+    let tempCal = 0;
+    // let personNum = 0;
+    arr.map((item) => {
+        if (item == '') {
+            calTracker.push(tempCal);
+            tempCal = 0;
+            // personNum++;
+            return;
+        }
+        tempCal += parseInt(item);
+    })
+
+    console.log(calTracker.sort((a, b) => a - b)[calTracker.length - 1] + calTracker.sort((a, b) => a - b)[calTracker.length - 2] + calTracker.sort((a, b) => a - b)[calTracker.length - 3]);
+
+})
